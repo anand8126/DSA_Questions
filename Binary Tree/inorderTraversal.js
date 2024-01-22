6ce67c1b34890562b1e43479d82ba384886375b7
Anand Prakash Chaudhary <apchaudhary8126@gmail.com>
2024-01-22 16:29:23 +0530

    first commit

--- Binary Tree/inorderTraversal.js
+++ Binary Tree/inorderTraversal.js
@@ -15,11 +15,71 @@ const inorderTraversal = (root) => {
   }
 };
 
-const root = new Node(1);
-root.left=new Node(2);
-root.right=new Node(3);
-root.left.left=new Node(4);
-root.left.right=new Node(5);
-root.right.left=new Node(6);
-root.right.right=new Node(7);
-inorderTraversal(root);
+const preOrderTraversal = (root) => {
+  if (root !== null) {
+    console.log(root.value);
+    preOrderTraversal(root.left);
+    preOrderTraversal(root.right);
+  }
+};
+
+const postOrderTraversal = (root) => {
+  if (root !== null) {
+    postOrderTraversal(root.left);
+    postOrderTraversal(root.right);
+    console.log(root.value);
+  }
+};
+
+const lengthOfTree = (root) => {
+  if (root === null) {
+    return 0;
+  }
+  const leftHeight = lengthOfTree(root.left);
+  const rightHeight = lengthOfTree(root.right);
+
+  if(leftHeight > rightHeight){
+    return leftHeight +1;
+  }
+  else {
+    return rightHeight +1;
+  }
+};
+
+
+const levelOrderTraversal = (root) =>{
+  if(root === null){
+    return root;
+  }
+
+  const ans=[];
+  const queue=[root];
+
+  while(queue.length > 0){
+    const current =queue.shift();
+    ans.push(current.value);
+
+    if(current.left){
+      queue.push(current.left);
+    }
+
+    if(current.right){
+      queue.push(current.right);
+    }
+  }
+  return ans;
+}
+
+const root = new Node(10);
+root.left = new Node(20);
+root.right = new Node(30);
+root.left.left = new Node(40);
+root.left.right = new Node(50);
+root.right.left = new Node(60);
+root.right.right = new Node(70);
+// inorderTraversal(root);
+// preOrderTraversal(root);
+// postOrderTraversal(root);
+// console.log("The Height of tree ",lengthOfTree(root));
+console.log("Leval Order Traversal ",levelOrderTraversal(root));
+
